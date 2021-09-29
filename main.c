82da01c61d8930d03fa6f99ec8abf3c3b3d0e492
jovialgang <89865845+jovialgang@users.noreply.github.com>
2021-09-29 15:11:06 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define maxx 87
+#define minn 64
+
+int main() {
+
+    int min_max;
+    scanf("%d", &min_max);
+    printf("Если входит в диапазон указанных значений, то выведется 1,\n если не входит, то 0\n%d  \n", min_max>= minn && min_max<= maxx);
+
+    int count;
+    scanf("%d", &count);
+    printf("Значение бита:\n%d", (count >> (13-1)) & 1);
+
+    return 0;
+}
