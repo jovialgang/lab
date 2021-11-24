946b7b1ab25cf05e4b17f0bcb050fd804a4d3a7a
jovialgang <89865845+jovialgang@users.noreply.github.com>
2021-11-25 01:28:36 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    scanf("%d", &i);
+
+    char *arr[11] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+    printf("Цифра, преобразованная в строку: %s\n\n", arr[i-1]);
+
+    int j;
+    int a = 0;
+    int d = 2;
+    scanf("%d", &j);
+    printf("Последовательность простых чисел: 2 ");
+    for (int c = 0; c < j-2; c++){
+        d++;
+        for (int p = 2; p < d; p++){
+            if (d % p == 0) break;
+            else if (d == p+1) printf(" %d ", d);
+
+        }
+
+
+    }
+    return 0;
+}
